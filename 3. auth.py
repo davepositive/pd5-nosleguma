b1184c4d1360057162191da8c18ad7f68ec186ab
davepositive <officialwoodyzepaynegames@gmail.com>
2026-05-20 01:37:06 +0300

    3. auth.py

---
+++ 3. auth.py
@@ -0,0 +1,17 @@
+from database import connect
+
+
+def login(username, password):
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM users WHERE username=? AND password=?",
+        (username, password)
+    )
+
+    user = cursor.fetchone()
+
+    conn.close()
+
+    return user
